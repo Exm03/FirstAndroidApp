9cbfaea867a7d972949fc58555a8118de6e60931
Yan Lagun <exm230600@gmail.com>
2026-02-08 00:54:34 +0100

    Build smart convertor

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.firstapp;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.firstapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+//    private ActivityMainBinding binding;
+    private Button btnConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,92 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnConvert = findViewById(R.id.btnConvert);
+
+        btnConvert.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to convert?");
+            builder.setCancelable(false);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ConvertSpeede();
+                }
+                    });
+            builder.setNegativeButton("No",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.setTitle("Convertation");
+            alertDialog.show();
+
+        });
+
+        btnConvert.setOnLongClickListener(v ->{
+            btnConvert.setTextColor(Color.RED);
+            return true;
+        });
+
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        binding.btnConvert.setOnClickListener(v ->
+//                binding.textResult.setText("clicked"));
+    }
+
+
+
+    public void ConvertSpeede() {
+        EditText user_data = findViewById(R.id.editUserText);
+        TextView lable_result = findViewById(R.id.textResult);
+
+        String data  = user_data.getText().toString();
+
+        if (!data.isEmpty()) {
+        float number =  Float.parseFloat(data);
+        number *= 1.60934f;
+        lable_result.setText(String.format("Result: %s", String.valueOf(number)));
+        } else {
+            btnConvert.setText("Error");
+            Toast.makeText(this, "Please enter a value", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
